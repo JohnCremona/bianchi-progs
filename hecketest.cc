700cb9f5387b0e3cf89ea725f0c376cb8e6e60be
John Cremona <john.cremona@gmail.com>
2022-01-27 16:54:22 +0000

    rewrite of hecketest to use NTL matrices consistently

--- hecketest.cc
+++ hecketest.cc
@@ -3,13 +3,17 @@
 #include <eclib/mmatrix.h>
 #include <NTL/mat_ZZ.h>
 #include <NTL/mat_poly_ZZ.h>
+//#include <NTL/pair_ZZX_long.h>
+#include <NTL/ZZXFactoring.h>
 #include "qidloop.h"
 #include "homspace.h"
 //#define LOOPER
 
 #define MAXPRIME 10000
 
-vector<bigint> char_poly(mat_m A, long denom=1, int show_factors=0); // using NTL
+mat_ZZ mat_to_mat_ZZ(mat A);
+
+ZZX scaled_charpoly(mat_ZZ A, const ZZ& den);
 
 // function to sort a factorization vector, first by degree of factor
 // then exponent of factor then lexicographically
@@ -35,7 +39,7 @@ int main(void)
 {
   long d, max(MAXPRIME);
  int np,ip,jp;
- Quad n; int mats, pols, facs, plusflag, cuspidal=1;
+ Quad n; int show_mats, show_pols, show_factors, plusflag, cuspidal=1;
  cerr << "Enter field (one of "<<valid_fields<<"): " << flush;  cin >> d;
  if (!check_field(d))
    {
@@ -46,9 +50,9 @@ int main(void)
  Quad::displayfield(cout);
  cerr << "Plus space (0/1)? "; cin>>plusflag;
  cerr << "Cuspidal subspace (0/1)? "; cin>>cuspidal;
- cerr << "See the hecke matrices (0/1)? "; cin >> mats;
- cerr << "See the char polys (0/1)? "; cin >> pols;
- cerr << "Factor the char polys (0/1)? "; cin >> facs;
+ cerr << "See the hecke matrices (0/1)? "; cin >> show_mats;
+ cerr << "See the char polys (0/1)? "; cin >> show_pols;
+ cerr << "Factor the char polys (0/1)? "; cin >> show_factors;
  Qideal N;
 #ifdef LOOPER
  QUINT firstn, lastn;
@@ -91,48 +95,54 @@ int main(void)
   vector<Quadprime> badprimes = h.N.factorization().sorted_primes();
   vector<Quadprime>::const_iterator pr;
   int nq = badprimes.size();
-  vector<bigint> charpol;
-  bigint MMODULUS(MODULUS);
   if (dim>0)
     {
-      mat_m id = (den*den)*idmat(int(dim));
-      mat_m wq(dim), wq2;
-      vector<mat_m> wqlist;
+      ZZX charpol; ZZ content; vec_pair_ZZX_long factors;
+      vector<mat_ZZ> tplist, wqlist;
       for (pr=badprimes.begin(); pr!=badprimes.end(); ++pr)
         {
           Quadprime Q = *pr;
           cout << "Computing W_"<<Q<<"..." << flush;
-          wq =  h.wop(Q,0,mats);
+          mat_ZZ wq =  mat_to_mat_ZZ(h.wop(Q,0,show_mats));
 	  cout << "done. " << flush;
-          // bigint lambda(den);
-          // int dimplus = addscalar(wq,-lambda).nullity();
-          // cout << "+1 eigenspace has dimension "<<dimplus<<endl;
-          // int dimminus = addscalar(wq,lambda).nullity();
-          // cout << "-1 eigenspace has dimension "<<dimminus<<endl;
-          charpol = char_poly(wq, den, facs&&!hmod);
-          // if (hmod)
-          //   for(vector<bigint>::iterator cpi = charpol.begin(); cpi!=charpol.end(); cpi++)
-          //     *cpi = mod(*cpi, hmod);
-          if (pols)
-            cout << "char poly coeffs = " << charpol;
-          cout << endl;
-          wq2 = matmulmodp(wq, wq, MMODULUS);
-	  if (wq2==id) cout << "Involution!" << "\n";
-	  else
+	  wqlist.push_back(wq);
+
+          charpol = scaled_charpoly(wq, to_ZZ(den));
+          if (show_pols)
             {
-              if(dim<20) cout << "wq^2 = " << wq2 << endl;
+              cout << "char poly coeffs = " << charpol << endl;
+            }
+          if(show_factors)
+            {
+              factor(content, factors, charpol);
+              ::sort(factors.begin(), factors.end(), fact_cmp);
+              cout<<"Factors are:"<<endl;
+              long nf = factors.length();
+              for(int i=0; i<nf; i++)
+                {
+                  cout<<(i+1)<<":\t"<<factors[i].a
+                      <<"\t(degree "<<deg(factors[i].a)<<")";
+                  cout<<"\t to power "<<factors[i].b;
+                  cout<<endl;
+                }
+            }
+          cout << endl;
+          if (IsDiag(power(wq,2), dim, to_ZZ(den*den)))
+            {
+              cout << "Involution!" << "\n";
+            }
+          else
+            {
               cout << "NOT an involution...." << "\n";
               exit(1);
             }
-	  wqlist.push_back(wq);
+
 	}
 #ifndef LOOPER
       cerr << "How many Hecke matrices T_p? ";
       cin >> np;
       cout<<endl;
 #endif
-      mat_m tp(dim), tpwq(dim), wqtp(dim);
-      vector<mat_m> tplist;
       ip=0;
       for (pr=Quadprimes::list.begin();
 	   pr!=Quadprimes::list.end() && ((pr-Quadprimes::list.begin())<np);
@@ -141,16 +151,35 @@ int main(void)
           Quadprime P = *pr;
 	  while (P.divides(N)) {++pr; P=*pr; np++;}
 	  cout << "Computing T_" << P << "..."<<flush;
-	  tp = h.heckeop(P,0,mats);
+	  mat_ZZ tp = mat_to_mat_ZZ(h.heckeop(P,0,show_mats));
 	  cout << "done. " << flush;
-          charpol = char_poly(tp, den, facs&&!hmod);
-          if (pols)
-            cout << "char poly coeffs = " << charpol;
-          cout<<endl;
+	  tplist.push_back(tp);
+
+          charpol = scaled_charpoly(tp, to_ZZ(den));
+          if (show_pols)
+            {
+              cout << "char poly coeffs = " << charpol << endl;
+            }
+          if(show_factors)
+            {
+              factor(content, factors, charpol);
+              ::sort(factors.begin(), factors.end(), fact_cmp);
+              cout<<"Factors are:"<<endl;
+              long nf = factors.length();
+              for(int i=0; i<nf; i++)
+                {
+                  cout<<(i+1)<<":\t"<<factors[i].a
+                      <<"\t(degree "<<deg(factors[i].a)<<")";
+                  cout<<"\t to power "<<factors[i].b;
+                  cout<<endl;
+                }
+            }
+          cout << endl;
+
 	  for (int kp=0; kp<nq; kp++)
 	    {
-              tpwq = matmulmodp(tp, wqlist[kp], MMODULUS);
-              wqtp = matmulmodp(wqlist[kp], tp, MMODULUS);
+              mat_ZZ tpwq = tp * wqlist[kp];
+              mat_ZZ wqtp = wqlist[kp] * tp;
 	      if (tpwq!=wqtp)
 	      {
 		cout << "Problem: T_"<<P
@@ -160,9 +189,9 @@ int main(void)
 	    }
 	  for (jp=0; jp<ip; jp++)
 	    {
-              tpwq = matmulmodp(tp, tplist[jp], MMODULUS);
-              wqtp = matmulmodp(tplist[jp], tp, MMODULUS);
-	      if (tpwq!=wqtp)
+              mat_ZZ tp1tp2 = tp * tplist[jp];
+              mat_ZZ tp2tp1 = tplist[jp] * tp;
+	      if (tp1tp2!=tp2tp1)
 		{
 		  cout << "Problem: T_"<<P
 		       <<" does not commute with T_P #" <<jp << "!\n";
@@ -177,7 +206,7 @@ int main(void)
 exit(0);
 }       // end of main()
 
-vector<bigint> char_poly(mat_m A,  long denom, int show_factors) // using NTL
+mat_ZZ mat_to_mat_ZZ(mat A)
 {
   int i, j, d = A.nrows();
 
@@ -186,49 +215,23 @@ vector<bigint> char_poly(mat_m A,  long denom, int show_factors) // using NTL
   ntl_A.SetDims(d,d);
   for(i=1; i<=d; i++)
     for(j=1; j<=d; j++)
-      ntl_A(i,j)=A(i,j);
+      ntl_A(i,j)=conv<ZZ>(A(i,j));
+  return ntl_A;
+}
 
-  // compute char poly in NTL:
-  ZZX ntl_cp;
-  CharPoly(ntl_cp, ntl_A);
-
-  // rescale if d>1:
-  if (denom>1)
+ZZX scaled_charpoly(mat_ZZ A, const ZZ& den)
+{
+  ZZX charpol;
+  CharPoly(charpol, A);
+  long d = deg(charpol);
+  if (den>1)
     {
-      // cout<<"Before rescaling, char poly = "<<ntl_cp<<endl;
       bigint dpow(1);
-      for(i=0; i<=d; i++)
+      for(int i=0; i<=d; i++)
         {
-          SetCoeff(ntl_cp, d-i, coeff(ntl_cp, d-i)/dpow);
-          dpow *= denom;
-        }
-      // cout<<"After rescaling, char poly = "<<ntl_cp<<endl;
-    }
-
-  // convert char poly back from NTL:
-  vector<bigint> cp(d+1);
-  for(i=0; i<=d; i++)
-    cp[i] = coeff(ntl_cp,i);
-
-  // compute and display factorization:
-
-  // NB the order the factors are found is random, so we sort them
-
-  if(show_factors)
-    {
-      vec_pair_ZZX_long factors;
-      ZZ cont;
-      factor(cont,factors,ntl_cp);
-      ::sort(factors.begin(), factors.end(), fact_cmp);
-      cout<<"Factors are:"<<endl;
-      long nf = factors.length();
-      for(i=0; i<nf; i++)
-        {
-          cout<<(i+1)<<":\t"<<factors[i].a
-              <<"\t(degree "<<deg(factors[i].a)<<")";
-          cout<<"\t to power "<<factors[i].b;
-          cout<<endl;
+          SetCoeff(charpol, d-i, coeff(charpol, d-i)/dpow);
+          dpow *= den;
         }
     }
-  return cp;
+  return charpol;
 }
