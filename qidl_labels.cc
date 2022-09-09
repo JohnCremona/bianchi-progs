d654095f4996eea2c95d85d23c24bdc2b61af58e
John Cremona <john.cremona@gmail.com>
2022-09-09 14:53:37 +0100

    added n-per-line option to qidl_labels utility

--- qidl_labels.cc
+++ qidl_labels.cc
@@ -8,20 +8,33 @@
 #include "primes.h"
 int main (int argc, char *argv[])
 {
-  if ( argc < 4 ) { puts ("Usage: qidl_labels d min_norm max_norm"); return 0; }
+  if ( (argc < 4) || (argc > 5) ) { puts ("Usage: qidl_labels d min_norm max_norm <n-per-line>"); return 0; }
   long d = atoi(argv[1]);
   long n1 = atoi(argv[2]);
   long n2 = atoi(argv[3]);
+  int n_per_line = 0; // default, no newlines except at the end
+  if (argc == 5)
+    n_per_line = atoi(argv[4]);
 
   Quad::field(d);
 
   Qidealooper loop_both(n1, n2, /* both conjugates */ 1, /* sorted */ 1);
+  int n=0;
   while( loop_both.not_finished() )
     {
       Qideal I = loop_both.next();
-      cout << ideal_label(I) << " ";
+      cout << ideal_label(I);
+      n++;
+      if ((n < n_per_line) || (n_per_line == 0))
+        cout << " ";
+      else
+        {
+          cout<<endl;
+          n = 0;
+        }
     }
-  cout << endl;
+  if (n)
+    cout << endl;
 }
 
 
