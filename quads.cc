91e91bd6a2871e9a779efcd40e92e1e940a85d7d
John Cremona <john.cremona@gmail.com>
2023-06-13 16:39:06 +0100

    added geodata for more fields

--- quads.cc
+++ quads.cc
@@ -41,8 +41,8 @@ vector<long> euclidean_fields = {1,2,3,7,11};
 vector<long> valid_fields = {1, 2, 3, 7, 11,                          // Euclidean
                              19, 43, 67, 163,                         // other class number 1
                              5, 6, 10, 13, 15, 22, 35, 37, 51, 91, 115, 123, 187, // class number 2 (incomplete)
-                             23, 31, 59, 83, 107, 139,                // class number 3 (incomplete)
-                             14, 17, 30, 34, 39, 46, 55, 155,         // class number 4 cyclic (incomplete)
+                             23, 31, 59, 83, 107, 139, 211,           // class number 3 (incomplete)
+                             14, 17, 30, 34, 39, 46, 55, 155, 203,    // class number 4 cyclic (incomplete)
                              21, 33, 42, 195,                         // class number 4 noncyclic (incomplete)
                              47, 79, 103, 127, 131, 179,              // class number 5 (incomplete)
                              26, 29, 38, 87,                          // class number 6 (incomplete)
@@ -51,7 +51,8 @@ vector<long> valid_fields = {1, 2, 3, 7, 11,                          // Euclide
                              199,                                     // class number 9 cyclic (incomplete)
                              119, 143, 159,                           // class number 10 cyclic (incomplete)
                              167,                                     // class number 11 (incomplete)
-                             191};                                    // class number 13 (incomplete)
+                             191,                                     // class number 13 (incomplete)
+                             215};                                    // class number 14 (incomplete)
 
 vector<long> class_number_one_fields   = {1,2,3,7,11,19,43,67,163};
 vector<long> class_number_two_fields   = {5,6,10,13,15,22,35,37,51,58,91,115,123,187,235,267,403,427};
