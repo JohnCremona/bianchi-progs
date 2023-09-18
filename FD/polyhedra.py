6ea825d28c874fa73b80ffaff6af6230c8348499
John Cremona <john.cremona@gmail.com>
2023-09-18 11:39:22 +0100

    added another polyhedron type

--- FD/polyhedra.py
+++ FD/polyhedra.py
@@ -70,6 +70,7 @@ DoubleHexagonalPrism = Graph([(0, 1), (0, 2), (0, 3), (4, 2), (4, 5), (4, 6), (7
                              name = "double hexagonal prism")
 
 # Half a stella octangula, also a square prism with two tetrahedra glued on non-adjacent triangles
+#  (includes a vertex of degree 6)
 # eg d=29, 53
 HalfStar = Graph( [(0, 1), (0, 2), (0, 3), (4, 5), (4, 6), (4, 3), (1, 0), (1, 2), (1, 5),
                    (1, 3), (5, 4), (5, 6), (5, 1), (5, 3), (2, 6), (2, 0), (2, 1), (2, 3),
@@ -101,10 +102,21 @@ PrismPyramid = Graph( [(0, 1), (0, 2), (0, 3), (0, 4), (2, 0), (2, 1), (2, 3), (
                       name = "triangular prism plus square pyramid")
 
 # tetrahedron + square pyramid
+# (includes a vertex of degree 5)
 # eg d=143
 TetraPyramid = Graph( [(0, 1), (0, 2), (0, 3), (0, 4), (4, 1), (4, 2), (4, 5), (4, 0), (2, 3), (2, 5),
-                       (2, 1), (2, 0), (2, 4), (1, 0), (1, 4), (1, 2), (5, 3), (5, 2), (5, 4), (3, 5), (3, 2), (3, 0)],
+                       (2, 1), (2, 0), (2, 4), (1, 0), (1, 4), (1, 2), (5, 3), (5, 2), (5, 4), (3, 5),
+                       (3, 2), (3, 0)],
                       name = "tetrahedron plus square pyramid")
+
+# 10 triangles + 2 squares
+# (includes a vertex of degree 6)
+# eg d=101
+Unnamed_1 = Graph( [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 5), (1, 6), (1, 4), (6, 5), (6, 7),
+                    (6, 1), (6, 3), (5, 6), (5, 7), (5, 4), (5, 1), (3, 2), (3, 8), (3, 0), (3, 6),
+                    (2, 3), (2, 4), (2, 8), (2, 0), (7, 4), (7, 5), (7, 6), (7, 8), (4, 5), (4, 7),
+                    (4, 8), (4, 2), (4, 0), (4, 1), (8, 2), (8, 4), (8, 3), (8, 7)],
+                   name = "unnamed #1")
 
 Unknown = Graph(name="unknown")
 
@@ -123,7 +135,7 @@ all_polys = (Tetrahedron, Cube, Octahedron,
              TriangularPrism, SquarePyramid, HexagonalPrism,
              HexagonalCap, TruncatedTetrahedron, Cuboctahedron,
              SlicedCube, DoubleHexagonalPrism, HalfStar, TentedHexPrism,
-             Dipyramid, PrismPyramid, TetraPyramid , Unknown)
+             Dipyramid, PrismPyramid, TetraPyramid , Unnamed_1, Unknown)
 
 def poly_type(pol):
     for G in all_polys:
