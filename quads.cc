dcb6f9ddbaf7d190f0bc10a46727c9f9452fafa8
John Cremona <john.cremona@gmail.com>
2023-09-20 11:58:46 +0100

    added field 463

--- quads.cc
+++ quads.cc
@@ -53,7 +53,7 @@ vector<long> valid_fields = {1, 2, 3, 7, 11,                           // Euclid
                              21, 30, 33, 42, 57, 70, 195, 435,        // class group C2xC2 (incomplete)
                              47, 79, 103, 127, 131, 179, 227, 347, 443,  // class group C5 (incomplete)
                              26, 29, 38, 53, 61, 87, 247, 339, 109, 411, 451, // class group C6 (incomplete)
-                             71, 151, 223, 251, 371, 467,             // class group C7 (incomplete)
+                             71, 151, 223, 251, 371, 463, 467,        // class group C7 (incomplete)
                              41, 62, 95, 111, 183, 295, 299, 94, 395, // class group C8 (incomplete)
                              65, 66, 69, 77,                          // class group C2xC4 (incomplete)
                              105,                                     // class group C2xC2xC2 (incomplete)
